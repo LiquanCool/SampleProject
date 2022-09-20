d65a97642ca498ec54ee68723fb16f1ee9ce6697
liquancool <llei0111@bths.edu>
2022-09-20 17:51:00 -0400

    Changed the movie and added a print statement for my favorite food

--- src/Sample.java
+++ src/Sample.java
@@ -1,6 +1,7 @@
 public class Sample {
     public static void main(String[] args) {
         System.out.print("Liquan");
-        System.out.print("Titanic");
+        System.out.print("Finding Nemo");
+        System.out.print("Eggs");
     }
 }
